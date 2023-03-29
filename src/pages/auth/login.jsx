f2ee74a9abfb062ce6eee583022b893818256083
Chukwudi Eze <chukwudieze97@gmail.com>
2023-03-29 10:35:56 +0100

    handle validation for auth

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,18 +1,65 @@
-import { Box, Button, Flex, Input, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-formid";
 import { Link as ReactLink } from "react-router-dom";
 
 const Login = () => {
+  const { handleSubmit, getFieldProps, errors } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validation: {
+      email: {
+        required: (val) => !!val || "Email address is required",
+        isValid: (val) => validator.isEmail(val) || "Email address is invalid",
+      },
+      password: {
+        required: (val) => !!val || "Password is required",
+        hasMoreThan6Chars: (val) =>
+          val.length >= 8 || "Please enter 8 or more characters",
+      },
+    },
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <Flex direction="column" gap="5">
-      <Box>
+    <Flex
+      as="form"
+      onSubmit={handleSubmit(onSubmit)}
+      direction="column"
+      gap="5"
+    >
+      <FormControl isInvalid={!!errors.email}>
         <Text>Email</Text>
-        <Input type="email" placeholder="Enter Email" />
-      </Box>
-      <Box>
+        <Input
+          type="email"
+          placeholder="Enter Email"
+          {...getFieldProps("email")}
+        />
+        {errors.email && <FormErrorMessage>{errors.email}</FormErrorMessage>}
+      </FormControl>
+      <FormControl isInvalid={!!errors.password}>
         <Text>Password</Text>
-        <Input type="password" placeholder="Enter Password" />
-      </Box>
+        <Input
+          type="password"
+          placeholder="Enter Password"
+          {...getFieldProps("password")}
+        />
+        {errors.password && (
+          <FormErrorMessage>{errors.password}</FormErrorMessage>
+        )}
+      </FormControl>
       <Box>
         <Text>
           Don't have an account?{" "}
@@ -22,7 +69,7 @@ const Login = () => {
         </Text>
       </Box>
       <Box>
-        <Button isLoading={false} colorScheme="teal">
+        <Button type="submit" isLoading={false} colorScheme="teal">
           Login
         </Button>
       </Box>
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,30 +1,113 @@
-import { Box, Button, Flex, Input, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-formid";
 import { Link as ReactLink } from "react-router-dom";
 
 const Register = () => {
+  const { handleSubmit, getFieldProps, errors } = useForm({
+    defaultValues: {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+    },
+    validation: {
+      first_name: {
+        required: true,
+      },
+      email: {
+        required: (val) => !!val || "Email address is required",
+        isValid: (val) => validator.isEmail(val) || "Email address is invalid",
+      },
+      last_name: {
+        required: true,
+      },
+      password: {
+        required: (val) => !!val || "Password is required",
+        hasMoreThan6Chars: (val) =>
+          val.length >= 8 || "Please enter 8 or more characters",
+      },
+      confirm_password: {
+        shouldMatch: (val, { password }) =>
+          val === password || "Passwords do not match",
+      },
+    },
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <Flex as="form" direction="column" gap="5">
-      <Box>
+    <Flex
+      as="form"
+      onSubmit={handleSubmit(onSubmit)}
+      direction="column"
+      gap="5"
+    >
+      <FormControl isInvalid={!!errors.first_name}>
         <Text>First Name</Text>
-        <Input type="text" placeholder="Enter First Name" />
-      </Box>
-      <Box>
+        <Input
+          type="text"
+          placeholder="Enter First Name"
+          {...getFieldProps("first_name")}
+        />
+        {errors.first_name && (
+          <FormErrorMessage>{errors.first_name}</FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isInvalid={!!errors.last_name}>
         <Text>Last Name</Text>
-        <Input type="text" placeholder="Enter First Name" />
-      </Box>
-      <Box>
+        <Input
+          type="text"
+          placeholder="Enter Last Name"
+          {...getFieldProps("last_name")}
+        />
+        {errors.last_name && (
+          <FormErrorMessage>{errors.last_name}</FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isInvalid={!!errors.email}>
         <Text>Email</Text>
-        <Input type="email" placeholder="Enter Email" />
-      </Box>
-      <Box>
+        <Input
+          type="email"
+          placeholder="Enter Email"
+          {...getFieldProps("email")}
+        />
+        {errors.email && (
+          <FormErrorMessage>{errors.email}</FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isInvalid={!!errors.password}>
         <Text>Password</Text>
-        <Input type="password" placeholder="Enter Password" />
-      </Box>
-      <Box>
+        <Input
+          type="password"
+          placeholder="Enter Password"
+          {...getFieldProps("password")}
+        />
+        {errors.password && (
+          <FormErrorMessage>{errors.password}</FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isInvalid={!!errors.confirm_password}>
         <Text>Confirm Password</Text>
-        <Input type="password" placeholder="Enter Password" />
-      </Box>
+        <Input
+          type="password"
+          placeholder="Enter Password"
+          {...getFieldProps("confirm_password")}
+        />
+        {errors.confirm_password && (
+          <FormErrorMessage>{errors.confirm_password}</FormErrorMessage>
+        )}
+      </FormControl>
       <Box>
         <Text>
           Already have an account?{" "}
@@ -34,7 +117,7 @@ const Register = () => {
         </Text>
       </Box>
       <Box>
-        <Button isLoading={false} colorScheme="teal">
+        <Button type="submit" isLoading={false} colorScheme="teal">
           Register
         </Button>
       </Box>
